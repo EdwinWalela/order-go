01b1f2728e71ace7eef56d4117ddc480d42a9d2c
Edwin Walela <edwinwalela@gmail.com>
2023-06-30 11:47:10 +0300

    Terraform config (#7)
    * define build job
    
    * define build job
    
    * define build job
    
    * define build job
    
    * define terraform service
    
    * define terraform service

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+resource "google_cloud_run_service" "api_service" {
+  name     = "go-orders"
+  location = "us-west1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/ics-jamii-1632986730022/go-order-api"
+
+        env {
+          name  = "FIRESTORE_PROJ_ID"
+          value = var.firestore_proj_id
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "firestore_proj_id" {
+  default = "firestore project id"
+}
